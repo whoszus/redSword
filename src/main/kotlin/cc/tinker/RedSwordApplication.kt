5ef39354833a32ec0e2fb716032309ee5fb70195
tinker <whoszus@yahoo.com>
2017-06-05 07:26:31 +0800

    Initial commit

---
+++ src/main/kotlin/cc/tinker/RedSwordApplication.kt
@@ -0,0 +1,11 @@
+package cc.tinker
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RedSwordApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(RedSwordApplication::class.java, *args)
+}
